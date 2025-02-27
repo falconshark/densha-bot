d139d2937817029f37e7bf2caabc0c11a851ebf6
Sardo <sardo.ip@sardo.work>
2025-02-27 17:32:44 +0900

    Able to migrate database

---
+++ alembic/versions/11dc7779531d_create_user_table.py
@@ -0,0 +1,37 @@
+"""create user table
+
+Revision ID: 11dc7779531d
+Revises: 
+Create Date: 2025-02-27 17:04:29.286654
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '11dc7779531d'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'user',
+        sa.Column('chat_id', sa.String(50), nullable=False, primary_key=True),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+    )
+    op.create_table(
+        'user_subscription',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('related_user', sa.String(50), nullable=False),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+    )
+    pass
+
+def downgrade() -> None:
+    op.drop_table('user')
+    pass
--- main.py
+++ main.py
@@ -10,10 +10,29 @@ load_dotenv(dotenv_path=dotenv_path)
 telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
 api_url = os.getenv('API_URL')
 
+print('Telegram ロボットが現在実行中です。')
+
+#Show route status by input route name.
 async def routeInfo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_input = update.message.text.split(' ')
     if len(user_input) < 2:
-        await update.message.reply_text('Please input the route name')
+        await update.message.reply_text('ルート名を入力してください。')
+        return
+    route_name = user_input[1]
+    route_info = api.load_densha_info(api_url, route_name)
+    route_status = route_info['results'][0]['route_status']
+    if(route_status == '通常'):
+        message = f'{route_name}は通常運転です'
+    else:
+        message = f'{route_name}は遅延しています'
+        
+    await update.message.reply_text(f'{message}')
+
+#Register Chat ID   
+async def routeInfo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_input = update.message.text.split(' ')
+    if len(user_input) < 2:
+        await update.message.reply_text('ルート名を入力してください。')
         return
     route_name = user_input[1]
     route_info = api.load_densha_info(api_url, route_name)
